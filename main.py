ed303e4faf1219de7d2911c92339fb725455ccd8
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2024-01-19 17:14:45 +0100

    changed the main.py, made the app open in centered form for a 1920x1080 dimension screen.

--- main.py
+++ main.py
@@ -7,5 +7,19 @@ if __name__ == "__main__":
     root = tk.Tk()
     app = SmartHomeGUI(root)
 
-    # Start the main GUI loop
+    window_width = 1100
+    window_height = 450
+
+    # Screen dimensions
+    screen_width = 1920
+    screen_height = 1080
+
+    # Calculate the center position
+    center_x = int((screen_width - window_width) / 2)
+    center_y = int((screen_height - window_height) / 2)
+
+    # Set the position of the window to the center of the screen
+    root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+
+    # Keep the window displaying
     root.mainloop()
